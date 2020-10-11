0ced3ced8bffbe9e7aaa6fcfa89144b037d763b3
Leonardo Alvarenga <leo.alvarenga08@hotmail.com>
2020-10-11 19:16:21 -0300

    Initial commit

---
+++ app/src/main/java/com/example/supermercado/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.supermercado;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lista;
+    private Button addButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lista = findViewById(R.id.lista);
+        addButton = findViewById(R.id.addButton);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addItem(view);
+            }
+        });
+
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        lista.setAdapter(itemsAdapter);
+        setUpListViewListener();
+    }
+
+    private void setUpListViewListener() {
+        lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Context context = getApplicationContext();
+                Toast.makeText(context, "Item Removed", Toast.LENGTH_LONG).show();
+
+                items.remove(i);
+                itemsAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), "Long Press to delete item...", Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+
+    private void addItem(View view) {
+        EditText nome = findViewById(R.id.editNome);
+        String nomeText = nome.getText().toString();
+
+
+        if(!(nomeText.equals(""))) {
+            itemsAdapter.add(nomeText);
+            nome.setText("");
+
+        } else {
+            Toast.makeText(getApplicationContext(), "Please enter text...", Toast.LENGTH_LONG).show();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/supermercado/Produto.java
@@ -0,0 +1,37 @@
+package com.example.supermercado;
+
+public class Produto {
+    private String nome;
+    private int quantidade;
+    private double valor;
+
+    public Produto(String nome, int quantidade, double valor) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+}
